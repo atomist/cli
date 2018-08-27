1c66294d3b516182b62c1423c2f8577c4c4d2b31
Christian Dupuis <cd@atomist.com>
2018-08-27 15:43:38 +0200

    Move postInstall.md to cli

--- bin/postInstall.js
+++ bin/postInstall.js
@@ -15,14 +15,15 @@
  * limitations under the License.
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-const automation_client_1 = require("@atomist/automation-client");
-const consoleOutput_1 = require("@atomist/sdm-local/src/cli/ui/consoleOutput");
+const automation_client = require("@atomist/automation-client");
+const doc_chunk = require("@atomist/sdm-local/src/cli/ui/docChunk");
 const fs = require("fs-extra");
 
 /* Adding the JS directly because we can't depend on TS files for the postInstall hook */
 
 // Show an informative and friendly welcome message
-if (!fs.existsSync(automation_client_1.userConfigPath())) {
-    consoleOutput_1.adviceDoc("docs/postInstall.md");
+if (!fs.existsSync(automation_client.userConfigPath())) {
+    var output = doc_chunk.renderProjectDocChunk("../../../docs/postInstall.md").trim();
+    process.stdout.write("\n" + output + "\n\n");
 }
 //# sourceMappingURL=postInstall.js.map
