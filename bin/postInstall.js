09bdade7ac895df7ca1aa09746d0360354a7fbcc
David Dooling <david@atomist.com>
2018-09-19 12:54:00 -0500

    Deal with errors in postInstall script
    Towards #36

--- bin/postInstall.js
+++ bin/postInstall.js
@@ -1,23 +1,49 @@
+#!/usr/bin/env node
 "use strict";
-/* Adding the JS directly because we can't depend on TS files for the postInstall hook */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const automation_client_1 = require("@atomist/automation-client");
 const fs = require("fs-extra");
 const marked = require("marked");
 const TerminalRenderer = require("marked-terminal");
-marked.setOptions({
-    // Define custom renderer
-    renderer: new TerminalRenderer(),
-});
-const Banner = `┌──────────────────────────────────────────────────────────────────────────┐
+function printErr(e) {
+    process.stderr.write(`@atomist/cli:postInstall [ERROR] ${e.message}\n`);
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            marked.setOptions({
+                // define custom renderer
+                renderer: new TerminalRenderer(),
+            });
+            const banner = `┌──────────────────────────────────────────────────────────────────────────┐
 │                                                                          │
 │   @atomist/cli is now installed.                                         │
 │                                                                          │
-│   Head to the sdm repo (https://github.com/atomist/sdm) for more info.   │
+│   Head to the SDM repo (https://github.com/atomist/sdm) for more info.   │
 │                                                                          │
 └──────────────────────────────────────────────────────────────────────────┘`;
-// Show an informative and friendly welcome message
-if (!fs.existsSync(automation_client_1.userConfigPath())) {
-    process.stdout.write(marked(Banner).trim() + "\n\n");
+            // show an informative and friendly welcome message
+            if (fs.existsSync(automation_client_1.userConfigPath())) {
+                process.stdout.write(marked(banner).trim() + "\n\n");
+            }
+        }
+        catch (e) {
+            printErr(e);
+        }
+    });
 }
+// we do not want any postInstall failure to cause install to fail
+main()
+    .catch(e => {
+    printErr(e);
+})
+    .then(() => process.exit(0), e => process.exit(0));
 //# sourceMappingURL=postInstall.js.map
