01ff189e723d8e0bb4881f0fffd03743be2a5e93
Christian Dupuis <cd@atomist.com>
2019-03-31 18:25:23 +0200

    Add support to config repos in GitHub provider

--- lib/provider/github.ts
+++ lib/provider/github.ts
@@ -83,16 +83,22 @@ const GitHubProviderQuery = `query ScmProviderById {
         value
       }
     }
+    credential {
+      secret
+    }
   }
 }`;
 
 interface GitHubProvider {
     SCMProvider: Array<{
         id: string;
-        targetConfiguration: Array<{ orgSpecs: string[] }>;
+        targetConfiguration: { orgSpecs: string[], repoSpecs: Array<{ ownerSpec: string, nameSpec: string }> };
         state: {
             error: string;
             name: string;
+        },
+        credential: {
+            secret: string;
         }
     }>;
 }
@@ -108,6 +114,7 @@ export async function createGitHubCom(workspaceId: string,
                                       cfg: Configuration): Promise<{ code: number }> {
     let providerId: string;
     let configuredOrgs: string[];
+    let configuredRepos: Array<{ ownerSpec: string, nameSpec: string }> = [];
 
     const graphClient = new ApolloGraphClient(`${cfg.endpoints.graphql}/${workspaceId}`,
         { Authorization: `Bearer ${apiKey}` });
@@ -122,12 +129,14 @@ export async function createGitHubCom(workspaceId: string,
         const provider = providerResult.SCMProvider[0];
         providerId = provider.id;
         configuredOrgs = _.get(providerResult, "SCMProvider[0].targetConfiguration.orgSpecs") || [];
+        configuredRepos = _.get(providerResult, "SCMProvider[0].targetConfiguration.repoSpecs") || [];
 
         if (!!provider.state && !!provider.state.error) {
-            print.log(`GitHub SCM provider is in state '${chalk.cyan(provider.state.name)}' with:
+            print.log(`GitHub SCM provider is in state ${chalk.cyan(provider.state.name)} with:
 ${chalk.red(provider.state.error)}`);
+        } else {
+            print.log(`GitHub SCM provider is in state ${chalk.cyan(provider.state.name)}`);
         }
-
     } else {
         spinner = createSpinner(`Creating new GitHub SCM provider`);
         const result = await graphClient.mutate<{ createGitHubResourceProvider: { id: string } }, {}>({
@@ -138,62 +147,77 @@ ${chalk.red(provider.state.error)}`);
         spinner.stop(true);
     }
 
-    spinner = createSpinner(`Redirecting through GitHub oauth to collect required secret`);
-    const state = nonce(20);
-    const verifier = nonce();
-    const code = sha256(verifier);
-    const port = await scanFreePort();
-    const authUrl = cfg.endpoints.auth;
-    const url = `${authUrl}/teams/${workspaceId}/resource-providers/${providerId}/token?code-challenge=${code}&state=${
-        state}&link=true&redirect-uri=${encodeURIComponent(`http://127.0.0.1:${port}/callback`)}`;
+    const secret = _.get(providerResult, "SCMProvider[0].credential.secret");
+    if (!secret) {
 
-    let redirectUrl;
-    const redirect = await axios.get(
-        url,
-        {
-            headers: { Authorization: `Bearer ${apiKey}` },
-            maxRedirects: 0,
-            validateStatus: status => status === 302,
-        },
-    );
-    redirectUrl = redirect.headers.location;
+        spinner = createSpinner(`Redirecting through GitHub oauth to collect required secret`);
+        const state = nonce(20);
+        const verifier = nonce();
+        const code = sha256(verifier);
+        const port = await scanFreePort();
+        const authUrl = cfg.endpoints.auth;
+        const url = `${authUrl}/teams/${workspaceId}/resource-providers/${providerId}/token?code-challenge=${code}&state=${
+            state}&link=true&redirect-uri=${encodeURIComponent(`http://127.0.0.1:${port}/callback`)}`;
 
-    await opn(redirectUrl, { wait: false });
-
-    const app = express();
-    const callback = new Deferred<{}>();
-    app.get("/callback", async (req, res) => {
-        if (state !== req.query.state) {
-            callback.reject("State parameter not correct after authentication. Abort!");
-            // res.status(500).json({ message: "State parameter not correct after authentication" });
-            res.redirect("https://atomist.com/error-oauth.html");
-            return;
-        }
-        try {
-            await axios.post(`${authUrl}/teams/${workspaceId}/resource-providers/${providerId}/token`, {
-                "code": req.query.code,
-                "code-verifier": verifier,
-            }, {
+        let redirectUrl;
+        const redirect = await axios.get(
+            url,
+            {
                 headers: { Authorization: `Bearer ${apiKey}` },
-            });
-            callback.resolve();
-            res.redirect("https://atomist.com/success-oauth.html");
-        } catch (e) {
-            callback.reject(new Error(`Authentication failed: ${e.message}`));
-            // res.status(500).json({ message: e.message });
-            res.redirect("https://atomist.com/error-oauth.html");
-        }
-    });
+                maxRedirects: 0,
+                validateStatus: status => status === 302 || status === 303,
+            },
+        );
+        redirectUrl = redirect.headers.location;
 
-    const server = app.listen(port);
-    try {
-        await callback.promise;
-    } finally {
-        server.close();
-        spinner.stop(true);
+        await opn(redirectUrl, { wait: false });
+
+        const app = express();
+        const callback = new Deferred<{}>();
+        app.get("/callback", async (req, res) => {
+            if (state !== req.query.state) {
+                callback.reject("State parameter not correct after authentication. Abort!");
+                // res.status(500).json({ message: "State parameter not correct after authentication" });
+                res.redirect("https://atomist.com/error-oauth.html");
+                return;
+            }
+            try {
+                await axios.post(`${authUrl}/teams/${workspaceId}/resource-providers/${providerId}/token`, {
+                    "code": req.query.code,
+                    "code-verifier": verifier,
+                }, {
+                    headers: { Authorization: `Bearer ${apiKey}` },
+                });
+                callback.resolve();
+                res.redirect("https://atomist.com/success-oauth.html");
+            } catch (e) {
+                callback.reject(new Error(`Authentication failed: ${e.message}`));
+                // res.status(500).json({ message: e.message });
+                res.redirect("https://atomist.com/error-oauth.html");
+            }
+        });
+
+        const server = app.listen(port);
+        try {
+            await callback.promise;
+        } finally {
+            server.close();
+            spinner.stop(true);
+        }
     }
 
-    await configure(workspaceId, apiKey, providerId, configuredOrgs, cfg);
+    const newProviderResult = await graphClient.query<GitHubProvider, {}>({
+        query: GitHubProviderQuery,
+    });
+
+    await configure(
+        workspaceId,
+        apiKey,
+        providerId,
+        configuredOrgs,
+        configuredRepos,
+        newProviderResult.SCMProvider[0].credential.secret,
+        cfg);
 
     return {
         code: 0,
@@ -204,9 +228,11 @@ async function configure(workspaceId: string,
                          apiKey: string,
                          providerId: string,
                          configuredOrgs: string[],
+                         configuredRepos: Array<{ ownerSpec: string, nameSpec: string }>,
+                         token: string,
                          cfg: Configuration): Promise<any> {
 
-    let spinner = createSpinner(`Loading GitHub organizations`);
+    let spinner = createSpinner(`Loading available GitHub organizations`);
 
     let graphClient = new ApolloGraphClient(
         cfg.endpoints.graphql.replace("/team", ""),
@@ -219,7 +245,11 @@ async function configure(workspaceId: string,
 
     const orgs = _.uniq([...configuredOrgs, ...accessibleOrgs.orgs.nodes.map(o => o.owner)]);
 
-    print.log(`Please select organizations you want to enable:`);
+    print.log("");
+    print.log(`You can now connect GitHub organizations and/or repositories to
+Atomist. When an organization or repository gets connected, Atomist
+will install a webhook to receive events like pushes, issues and PRs.`);
+    print.log("");
     const questions: inquirer.Question[] = [{
         type: "checkbox",
         name: "orgs",
@@ -230,6 +260,34 @@ async function configure(workspaceId: string,
                 value: o,
                 checked: configuredOrgs.includes(o),
             })),
+    }, {
+        type: "checkbox",
+        name: "repos",
+        message: "Repositories",
+        choices: [{ name: "Connect new repository", value: "<new_repo>" },
+            new inquirer.Separator(),
+            ...configuredRepos.sort((o1, o2) =>
+                `${o1.ownerSpec}/${o1.nameSpec}`.localeCompare(`${o2.ownerSpec}/${o2.nameSpec}`))
+                .map(o => ({
+                    name: `${o.ownerSpec}/${o.nameSpec}`,
+                    value: o,
+                    checked: true,
+                }))],
+    }, {
+        type: "input",
+        name: "newRepo",
+        message: "Connect repository (owner/repo)",
+        when: a => a.repos.includes("<new_repo>"),
+        validate: async input => {
+            try {
+                await axios.get(
+                    `https://api.github.com/repos/${input}`,
+                    { headers: { Authorization: `token ${token}` } });
+                return true;
+            } catch (e) {
+                return chalk.red(`Repository ${input} does not exist or you don't have permissions to access it`);
+            }
+        },
     }];
     const answers = await inquirer.prompt(questions);
 
@@ -237,13 +295,28 @@ async function configure(workspaceId: string,
 
     graphClient = new ApolloGraphClient(`${cfg.endpoints.graphql}/${workspaceId}`,
         { Authorization: `Bearer ${apiKey}` });
+
+    const repos = (!answers.repos ? [] : answers.repos).filter((r: any) => r !== "<new_repo>").map((r: any) => ({
+        owner: r.ownerSpec,
+        repo: r.nameSpec,
+    }));
+
+    if (!!answers.newRepo) {
+        repos.push(slugToRepoSpec(answers.newRepo));
+    }
+
     await graphClient.mutate<{}, { id: string, orgs: string[], repos: Array<{ owner: string, repo: string }> }>({
         mutation: ConfigureGitHubScmProviderMutation,
         variables: {
             id: providerId,
             orgs: !answers.orgs ? [] : answers.orgs,
-            repos: [],
+            repos,
         },
     });
     spinner.stop(true);
+}
+
+function slugToRepoSpec(slug: string): { owner: string, repo: string } {
+    const parts = slug.split("/");
+    return { owner: parts[0], repo: parts[1] };
 }
