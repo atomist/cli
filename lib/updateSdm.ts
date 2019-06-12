3c8cdc43287f2b2a3e6220a996bbbde106eeb653
Lieven Doclo <lieven@atomist.com>
2019-06-12 15:11:54 +0200

    Add command to update SDM atomist dependencies

---
+++ lib/updateSdm.ts
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2019 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { execPromise } from "@atomist/sdm";
+import * as fs from "fs-extra";
+import * as path from "path";
+import { createSpinner } from "./config";
+import * as print from "./print";
+
+/**
+ * Command-line options and arguments for install.
+ */
+export interface InstallOptions {
+    /** Name or keywords to install for */
+    versionTag: string;
+    /** Directory to run command in, must be an SDM directory */
+    cwd?: string;
+}
+
+function getAtomistDependencies(dependencies: any): string[] {
+    return Object.keys(dependencies).filter(key => key.startsWith("@atomist/"));
+}
+
+function hasPackageJson(directory: string): boolean {
+    return fs.existsSync(path.join(directory, "package.json"));
+}
+
+async function updateDependenciesToTag(dependencies: string[], versionTag: string, spinner: any): Promise<void> {
+    await Promise.all(dependencies.map(async dep => {
+        await execPromise(
+            "npm",
+            [
+                "install",
+                "--save",
+                `${dep}@${versionTag}`]);
+        spinner.setSpinnerTitle(`Updated ${dep} development dependency to ${versionTag}`);
+    }));
+}
+
+async function updateDevDependenciesToTag(dependencies: string[], versionTag: string, spinner: any): Promise<void> {
+    await Promise.all(dependencies.map(async dep => {
+        await execPromise(
+            "npm",
+            [
+                "install",
+                "--save-dev",
+                `${dep}@${versionTag}`]);
+        spinner.setSpinnerTitle(`Updated ${dep} dependency to ${versionTag}`);
+    }));
+}
+
+/**
+ * Search for an SDM extension pack.
+ *
+ * @param opts see InstallOptions
+ * @return integer return value
+ */
+export async function updateSdm(opts: InstallOptions): Promise<number> {
+    if (hasPackageJson(opts.cwd)) {
+        const spinner = createSpinner(
+            `Updating dependencies in package.json`);
+        const packageJson = await fs.readJson(path.join(opts.cwd, "package.json"));
+        const extractedDependencies = getAtomistDependencies(packageJson.dependencies);
+        const extractedDevDependencies = getAtomistDependencies(packageJson.devDependencies);
+        await updateDependenciesToTag(extractedDependencies, opts.versionTag, spinner);
+        await updateDevDependenciesToTag(extractedDevDependencies, opts.versionTag, spinner);
+        spinner.stop(true);
+        return 0;
+    } else {
+        print.error(`Current directory does not contain a package.json`);
+        return 1;
+    }
+}
